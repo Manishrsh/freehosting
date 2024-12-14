317277319a22f9267d4b8c377b160d1aa41a34e8
Manishrsh <manishrs5235@gmail.com>
2024-12-14 14:13:59 +0530

    manish

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -4,13 +4,19 @@ import { Github, Twitter, Linkedin, X } from "lucide-react";
 interface FormData {
   githubUrl: string;
   siteName: string;
+  fileName:string;
 }
 
 const RegistrationForm = ({ onGetExited }: { onGetExited: () => void }) => {
+  const [AddFileName , setAddFileName] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     githubUrl: "",
     siteName: "",
+    fileName:""
   });
+
+
+
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -79,13 +85,53 @@ const RegistrationForm = ({ onGetExited }: { onGetExited: () => void }) => {
               pattern="[a-z0-9-]+"
               title="Only lowercase letters, numbers, and hyphens are allowed"
             />
+
           </div>
-          <button
-            type="submit"
-            className="w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white py-3 rounded-lg font-semibold hover:opacity-90 transition-opacity"
-          >
-            Submit
-          </button>
+          {
+              AddFileName && (
+                <div>
+                  <label
+                    htmlFor="fileName"
+                    className="block text-sm font-medium text-gray-300 mb-2"
+                  >
+                    File Name
+                  </label>
+                  <input
+                    type="text"
+                    id="fileName"
+                    name="fileName"
+                    value={formData.fileName}
+                    onChange={handleChange}
+                    placeholder="app.html"
+                    className="w-full px-4 py-2 bg-zinc-800 border border-zinc-700 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent"
+                    required
+                    pattern="[a-z0-9-]+"
+                    title="Only lowercase letters, numbers, and hyphens are allowed"
+                  />
+                </div>
+              )
+
+          }
+          
+          
+          <div className="flex flex-col items-center space-y-4">
+  <button
+    type="submit"
+    className="w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white py-3 rounded-lg font-semibold hover:opacity-90 transition-opacity"
+  >
+    Submit
+  </button>
+  <div className="w-full flex justify-end">
+    <button
+      type="submit"
+      onClick={() => setAddFileName(!AddFileName)}
+      className="w-2/4 bg-gradient-to-r from-zinc-700 to-slate-800 text-white py-3 rounded-lg font-semibold hover:opacity-90 transition-opacity"
+    >
+      Only ( Html , Css , Js )
+    </button>
+  </div>
+</div>
+
         </form>
       </div>
     </div>
